d0e8443f15ceecf441083235893861ad1a3942eb
Rafael <rafael.miric@gmail.com>
2024-11-28 12:05:00 +0100

    Start djagno project

---
+++ moje_financije/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for moje_financije project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'moje_financije.settings')
+
+application = get_wsgi_application()
